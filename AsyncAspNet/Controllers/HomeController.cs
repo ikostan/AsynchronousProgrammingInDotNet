5dd3ee95b64d2292efb33e91f79ebb074c0baafa
ikostan <igorkostan@gmail.com>
2018-02-10 22:52:29 -0800

    Async for ASP.Net

--- AsyncAspNet/Controllers/HomeController.cs
+++ AsyncAspNet/Controllers/HomeController.cs
@@ -6,16 +6,25 @@ namespace AsyncAspNet.Controllers
 {
     public class HomeController : Controller
     {
+        /*
+        //Simple method
+        public ActionResult Index()
+        {
+            return Content("Hello World!");
+        }
+        */
+      
         public async Task<ActionResult> Index()
         {
             using (var client = new HttpClient())
             {
-                var httpMessage = await client.GetAsync("http://www.filipekberg.se/rss/").ConfigureAwait(false);
+                var httpMessage = await client.GetAsync("http://www.filipekberg.se/rss/"); //.ConfigureAwait(false);
+                //var httpMessage = await client.GetAsync("http://www.cic.gc.ca/english/helpcentre/answer.asp?qnum=1181&top=15").ConfigureAwait(false);
 
-                var content = await httpMessage.Content.ReadAsStringAsync().ConfigureAwait(false);
+                var content = await httpMessage.Content.ReadAsStringAsync(); //.ConfigureAwait(false);
 
                 return Content(content);
             }
-        }
+        }      
     }
 }
